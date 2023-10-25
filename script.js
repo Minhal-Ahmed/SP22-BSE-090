33e491403953b0e9aa100d8b8e48413819488913
Minhal-Ahmed <minhalahmed28@gmail.com>
2023-10-25 16:33:23 +0500

    Commit2

--- script.js
+++ script.js
@@ -4,40 +4,14 @@ tog.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
     })
-})
+
+});
 
 //deletion
-const first=document.querySelectorAll('.faq');
-first=onclick('remove');
-
-document.getElementsByClassName("faq");
-
-function myFunction() {
-    var element = document.getElementById("myDIV");
-    element.classList.remove("mystyle");
+function removefaq() {
+    var element = document.getElementById('faq');
+    element.classList.remove('faq');
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const toggles = document.querySelectorAll('.faq-toggle')
-
-toggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-        toggle.parentNode.classList.toggle('active')
-    })
-})
-
-*/
+document.getElementsByClassName("faq");
+console.log(removefaq);
